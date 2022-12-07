14bede41312cb29c6126ed485649389c6c932f68
Thomas Chamberlin <tchamber@nrao.edu>
2022-12-07 15:50:18 -0500

    Added configurable timing threshold
    Switched from time.monotonic to time.perf_counter
    
    Threshold defaults to 1 second (i.e. any cell that takes longer than 1 second will show timing info)
    
    Threshold can be modified via %threshold <num_seconds>

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # ipython-autotime
-Time everything in IPython
+Time cells in IPython that take longer than 1 second (value is configurable)
 
 ## Installation:
 
 ```console
-$ pip install ipython-autotime
+$ pip install git+https://github.com/tchamberlin/ipython-autotime
+```
+
+To auto-start with IPython, see https://ipython.readthedocs.io/en/stable/config/intro.html#setting-configurable-options
+
+Example:
+
+```shell
+# Create default config file
+ipython profile create
+# modify config file
+vim ~/.ipython/profile_default/ipython_config.py
+# Set: c.InteractiveShellApp.extensions = ["autotime"] 
 ```
 
 ## Examples
@@ -24,6 +36,21 @@ ZeroDivisionError                         Traceback (most recent call last)
 
 ZeroDivisionError: division by zero
 time: 88.7 ms (started: 2020-12-15 11:44:53 +01:00)
+
+In [4]: import time
+
+In [5]: time.sleep(0.1)
+
+In [6]: time.sleep(1.1)
+time: 1.1 s (started: 2022-12-07 15:57:14 -05:00)
+
+In [7]: %threshold 0.5
+
+In [8]: time.sleep(1.1)
+time: 1.1 s (started: 2022-12-07 15:57:31 -05:00)
+
+In [9]: time.sleep(0.1)
+
 ```
 
 ## Want to turn it off?
--- autotime/__init__.py
+++ autotime/__init__.py
@@ -4,41 +4,56 @@ from ._version import version as __version__
 
 from time import strftime, localtime
 
-try:
-    from time import monotonic
-except ImportError:
-    from monotonic import monotonic
+from time import perf_counter
 
 from IPython.core.magics.execution import _format_time as format_delta
 
+from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
+
 
 def format_timestamp(struct_time):
-    timestamp = strftime('%Y-%m-%d %H:%M:%S %z', struct_time)
+    timestamp = strftime("%Y-%m-%d %H:%M:%S %z", struct_time)
     # add colon in %z (for datetime.fromisoformat, stackoverflow.com/q/44836581)
-    return '{}:{}'.format(timestamp[:-2], timestamp[-2:])
+    return "{}:{}".format(timestamp[:-2], timestamp[-2:])
 
 
-class LineWatcher(object):
+@magics_class
+class LineWatcher(Magics):
     """Class that implements a basic timer.
 
     Notes
     -----
     * Register the `start` and `stop` methods with the IPython events API.
     """
-    __slots__ = ['start_time', 'timestamp']
+
+    __slots__ = ["start_time", "timestamp"]
+
+    @line_magic
+    def threshold(self, line):
+        try:
+            self._threshold = float(line)
+        except ValueError as error:
+            raise ValueError(
+                "Threshold should be set as a number of seconds (float)"
+            ) from error
+
+    def __init__(self, *args, threshold=1, **kwargs):
+        self._threshold = threshold
+        super().__init__(*args, **kwargs)
 
     def start(self):
         self.timestamp = localtime()
-        self.start_time = monotonic()
+        self.start_time = perf_counter()
 
     def stop(self):
-        delta = monotonic() - self.start_time
-        print(
-            u'time: {} (started: {})'.format(
-                format_delta(delta),
-                format_timestamp(self.timestamp),
+        delta = perf_counter() - self.start_time
+        if delta > self._threshold:
+            print(
+                "time: {} (started: {})".format(
+                    format_delta(delta),
+                    format_timestamp(self.timestamp),
+                )
             )
-        )
 
 
 timer = LineWatcher()
@@ -47,12 +62,12 @@ stop = timer.stop
 
 
 def load_ipython_extension(ip):
+    ip.register_magics(LineWatcher)
     start()
-    ip.events.register('pre_run_cell', start)
-    ip.events.register('post_run_cell', stop)
+    ip.events.register("pre_run_cell", start)
+    ip.events.register("post_run_cell", stop)
 
 
 def unload_ipython_extension(ip):
-    ip.events.unregister('pre_run_cell', start)
-    ip.events.unregister('post_run_cell', stop)
-
+    ip.events.unregister("pre_run_cell", start)
+    ip.events.unregister("post_run_cell", stop)
